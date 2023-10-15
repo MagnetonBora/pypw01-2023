261a2d38098f811f988824dca45e9d262cf67535
Roman Dryndik <dryndikroman@gmail.com>
2023-10-15 13:05:50 +0200

    Added basic dictionaries example

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+phones = {}
+while True:
+    command = input("Peek an option: new_contact, show_contacts ")
+    if command == "new_contact":
+        full_name = input("Input full name: ")
+        phone_num = input("Input phone number: ")
+        phones[full_name] = phone_num
+    elif command == "show_contacts":
+        print(phones)
+    # else:
+    #     print("Unknown command")
+
+    command = input("Do you want to continue Y/N? ")
+    if command == "N":
+        break
