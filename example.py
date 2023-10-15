d0fb9cfbd8084c22536c29f74be9f959978cf747
Roman Dryndik <dryndikroman@gmail.com>
2023-10-15 10:45:59 +0200

    Refactor

--- example.py
+++ example.py
@@ -1,8 +1,5 @@
 def sweap_max(items: list) -> list:
-    max_pos = 0
-    for i in range(len(items)):
-        if items[i] >= items[max_pos]:
-            max_pos = i
+    max_pos = items.index(max(items))
     items[0], items[max_pos] = items[max_pos], items[0]
     return items
 
